ce122ffc08de4a8d4de0fcdf5744c37ee9ee5050
Chris Mark <chdmark@gmail.com>
2016-02-11 21:42:44 -0800

    Add brute force solution.

---
+++ get-product-int-except-index.rb
@@ -0,0 +1,22 @@
+
+#brute fore solution
+def get_products_of_all_ints_except_index(array)
+  
+  product_array = []
+    
+    array.each do |element|
+      counter = 1
+      array.each do |another_element|
+        if another_element != element
+          counter *= another_element
+        end
+    end
+      product_array << counter
+    end
+
+  product_array
+      
+end
+
+test = [1,7,3,4]
+p get_products_of_all_ints_except_index(test)
